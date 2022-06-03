12e004e9c0d22567a37669aa57d7b1440512e141
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-06-03 15:28:14 -0300

    outline homes

--- src/app/components/homes/homes.component.ts
+++ src/app/components/homes/homes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, of } from 'rxjs';
+import { IHome } from './interfaces/IHome';
 
 @Component({
   selector: 'app-homes',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homes.component.css']
 })
 export class HomesComponent implements OnInit {
+  homes = new BehaviorSubject<IHome[]>([]);
+  homes$ = this.homes.asObservable();
+
 
   constructor() { }
 
   ngOnInit(): void {
+    // this.homes$ = this.dataService.getHomes$();
+    this.homes$ = of([ //Mocks
+      {
+        title: 'Home 1',
+        image: 'assets/listing.jpg',
+        location: 'New York'
+      },
+      {
+        title: 'Home 2',
+        image: 'assets/listing.jpg',
+        location: 'Boston'
+      },
+      {
+        title: 'Home 3',
+        image: 'assets/listing.jpg',
+        location: 'Chicagp'
+      },
+    ]);
   }
 
 }
